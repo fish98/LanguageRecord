d5fa9c764a795f2e241eb27396ab87c408584271
Yang Keao <keao.yang@yahoo.com>
2018-09-05 21:46:22 +0800

    FIrst Commit

---
+++ src/main.js
@@ -0,0 +1,353 @@
+import { Terminal, Line, inputInterval, ASCIIArt, Choice, Wait } from './terminal'
+
+import scriptRaw from './script'
+let script = scriptRaw
+
+const term = new Terminal('#terminal')
+
+window.addEventListener("load", () => {
+  const  next = (i) => {
+    const now = script[i]
+    if (now.type === 'inputInterval') {
+      term.insert(new inputInterval("<color1> QSC </color1>", now.content, () => {
+        next(i+1)
+      }))
+    } else if (now.type === 'ASCIIArt') {
+      term.insert(new ASCIIArt(now.content))
+      next(i+1)
+    } else if (now.type === 'choice') {
+      const choice = new Choice(" QSC ", now.content)
+      choice.onChoose = (num) => {
+        choice.onChoose = () => {}
+        if (now.event[num]) {
+          script = now.event[num]
+        } else {
+          now.event.forEach(item => {
+            script = item
+          })
+        }
+        next(0)
+      }
+      term.insert(choice)
+    } else if (now.type === 'interval') {
+      term.insert(new inputInterval("", now.content, () => {
+        next(i+1)
+      }))
+    } else if (now.type === 'wait') {
+      term.insert(new Wait(now.content, () => {
+        next(i+1)
+      }))
+    } else if (now.type === 'Final') {
+      term.insert(new ASCIIArt(`QSys version 4.19-rc2 (wcc version 7.3.0)`))
+      term.insert(new ASCIIArt(`
+e820: BIOS-provided physical RAM map: <br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000000000000-0x0000000000000fff] reserved<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000000001000-0x0000000000057fff] usable<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000000058000-0x0000000000058fff] reserved<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000000059000-0x000000000009cfff] usable<br>
+<i class="color3">BIOS-e820</i>: [mem 0x000000000009d000-0x000000000009ffff] reserved<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000000100000-0x000000006659dfff] usable<br>
+<i class="color3">BIOS-e820</i>: [mem 0x000000006659e000-0x000000006659efff] ACPI NVS<br>
+<i class="color3">BIOS-e820</i>: [mem 0x000000006659f000-0x00000000665c8fff] reserved<br>
+<i class="color3">BIOS-e820</i>: [mem 0x00000000665c9000-0x0000000066624fff] usable<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000066625000-0x0000000066e15fff] reserved<br>
+<i class="color3">BIOS-e820</i>: [mem 0x0000000066e16000-0x0000000077c82fff] usable<br>
+`))
+      setTimeout(() => {
+        term.insert(new ASCIIArt(`Security Framework initialized`))
+        setTimeout(() => {
+          term.insert(new ASCIIArt(`Trying to unpack rootfs image as initramfs...`))
+          setTimeout(() => {
+            term.insert(new ASCIIArt(`Calculator loaded`))
+            setTimeout(() => {
+              term.insert(new ASCIIArt(`ACPI: Power (Electronic) on`))
+              setTimeout(() => {
+                term.insert(new ASCIIArt(`ACPI: Switch to nuclear power`))
+                setTimeout(() => {
+                  term.insert(new ASCIIArt(`Initialise system trusted keyrings`))
+                  setTimeout(() => {
+                    term.insert(new ASCIIArt(`WELCOME TO PLAN 10 DEMO`))
+                    setTimeout(() => {
+                      term.insert(new ASCIIArt(`
+                        LANG=zh_CN.utf8 <br>
+                        LC_CTYPE=zh_CN.utf8 <br>
+                        LC_NUMERIC="zh_CN.utf8" <br>
+                        LC_TIME="zh_CN.utf8" <br>
+                        LC_COLLATE="zh_CN.utf8" <br>
+                        LC_MONETARY="zh_CN.utf8" <br>
+                        LC_MESSAGES="zh_CN.utf8" <br>
+                        LC_PAPER="zh_CN.utf8" <br>
+                        LC_NAME="zh_CN.utf8" <br>
+                        LC_ADDRESS="zh_CN.utf8" <br>
+                        LC_TELEPHONE="zh_CN.utf8" <br>
+                        LC_MEASUREMENT="zh_CN.utf8" <br>
+                        LC_IDENTIFICATION="zh_CN.utf8" <br>
+                        欢迎使用 PLAN 10 测试系统 <br>
+                      `))
+                      setTimeout(() => {
+                        term.insert(new ASCIIArt(`
+                          正在演算
+                        `))
+                        setTimeout(() => {
+                          term.insert(new ASCIIArt(`
+                            <i class="color2">[WW]</i> QPU0: 温度过高，限制频率 <br>
+                            <i class="color2">[WW]</i> QPU1: 温度过高，限制频率 <br>
+                            <i class="color2">[WW]</i> QPU2: 温度过高，限制频率 <br>
+                            <i class="color2">[WW]</i> QPU3: 温度过高，限制频率 <br>
+                          `))
+                          setTimeout(() => {
+                            term.insert(new ASCIIArt(`
+                              [II] QPU0: 温度/频率恢复正常 <br>
+                              [II] QPU1: 温度/频率恢复正常 <br>
+                              [II] QPU2: 温度/频率恢复正常 <br>
+                              [II] QPU3: 温度/频率恢复正常 <br>
+                            `))
+                            setTimeout(() => {
+                              term.insert(new ASCIIArt(`
+                                <i class="color2">[WW]</i> QPU0: 温度过高，限制频率 <br>
+                                <i class="color2">[WW]</i> QPU1: 温度过高，限制频率 <br>
+                                <i class="color2">[WW]</i> QPU2: 温度过高，限制频率 <br>
+                                <i class="color2">[WW]</i> QPU3: 温度过高，限制频率 <br>
+                              `))
+                              setTimeout(() => {
+                                term.insert(new ASCIIArt(`
+                                  [II] QPU0: 温度/频率恢复正常 <br>
+                                  [II] QPU1: 温度/频率恢复正常 <br>
+                                  [II] QPU2: 温度/频率恢复正常 <br>
+                                  [II] QPU3: 温度/频率恢复正常 <br>
+                                `))
+                                setTimeout(() => {
+                                  console.log(now)
+                                  term.insert(new ASCIIArt(`
+                                    已得出运算结果：${now.lang}
+                                  `))
+                                  const termEle = document.getElementById('terminal')
+                                  if (now.lang === 'C++') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+ ######
+##    ##   ##     ##
+##         ##     ##
+##       ###### ######
+##         ##     ##
+##    ##   ##     ##
+ ######
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'C') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+ ######
+##    ##
+##
+##
+##
+##    ##
+ ######
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Rust') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########  ##     ##  ######  ########
+##     ## ##     ## ##    ##    ##
+##     ## ##     ## ##          ##
+########  ##     ##  ######     ##
+##   ##   ##     ##       ##    ##
+##    ##  ##     ## ##    ##    ##
+##     ##  #######   ######     ##
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Java') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+      ##    ###    ##     ##    ###
+      ##   ## ##   ##     ##   ## ##
+      ##  ##   ##  ##     ##  ##   ##
+      ## ##     ## ##     ## ##     ##
+##    ## #########  ##   ##  #########
+##    ## ##     ##   ## ##   ##     ##
+ ######  ##     ##    ###    ##     ##
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Python') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########  ##    ## ######## ##     ##  #######  ##    ##
+##     ##  ##  ##     ##    ##     ## ##     ## ###   ##
+##     ##   ####      ##    ##     ## ##     ## ####  ##
+########     ##       ##    ######### ##     ## ## ## ##
+##           ##       ##    ##     ## ##     ## ##  ####
+##           ##       ##    ##     ## ##     ## ##   ###
+##           ##       ##    ##     ##  #######  ##    ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'JavaScript') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+      ##    ###    ##     ##    ###     ######   ######  ########  #### ########  ########
+      ##   ## ##   ##     ##   ## ##   ##    ## ##    ## ##     ##  ##  ##     ##    ##
+      ##  ##   ##  ##     ##  ##   ##  ##       ##       ##     ##  ##  ##     ##    ##
+      ## ##     ## ##     ## ##     ##  ######  ##       ########   ##  ########     ##
+##    ## #########  ##   ##  #########       ## ##       ##   ##    ##  ##           ##
+##    ## ##     ##   ## ##   ##     ## ##    ## ##    ## ##    ##   ##  ##           ##
+ ######  ##     ##    ###    ##     ##  ######   ######  ##     ## #### ##           ##                        
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Go') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+ ######    #######
+##    ##  ##     ##
+##        ##     ##
+##   #### ##     ##
+##    ##  ##     ##
+##    ##  ##     ##
+ ######    #######                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Swift') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+ ######  ##      ## #### ######## ########
+##    ## ##  ##  ##  ##  ##          ##
+##       ##  ##  ##  ##  ##          ##
+ ######  ##  ##  ##  ##  ######      ##
+      ## ##  ##  ##  ##  ##          ##
+##    ## ##  ##  ##  ##  ##          ##
+ ######   ###  ###  #### ##          ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Dart') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########     ###    ########  ########
+##     ##   ## ##   ##     ##    ##
+##     ##  ##   ##  ##     ##    ##
+##     ## ##     ## ########     ##
+##     ## ######### ##   ##      ##
+##     ## ##     ## ##    ##     ##
+########  ##     ## ##     ##    ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Ceylon') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+ ######  ######## ##    ## ##        #######  ##    ##
+##    ## ##        ##  ##  ##       ##     ## ###   ##
+##       ##         ####   ##       ##     ## ####  ##
+##       ######      ##    ##       ##     ## ## ## ##
+##       ##          ##    ##       ##     ## ##  ####
+##    ## ##          ##    ##       ##     ## ##   ###
+ ######  ########    ##    ########  #######  ##    ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'PHP') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########  ##     ## ########
+##     ## ##     ## ##     ##
+##     ## ##     ## ##     ##
+########  ######### ########
+##        ##     ## ##
+##        ##     ## ##
+##        ##     ## ##
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Perl 6') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########  ######## ########  ##           #######
+##     ## ##       ##     ## ##          ##     ##
+##     ## ##       ##     ## ##          ##
+########  ######   ########  ##          ########
+##        ##       ##   ##   ##          ##     ##
+##        ##       ##    ##  ##          ##     ##
+##        ######## ##     ## ########     #######                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Erlang') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+######## ########  ##          ###    ##    ##  ######
+##       ##     ## ##         ## ##   ###   ## ##    ##
+##       ##     ## ##        ##   ##  ####  ## ##
+######   ########  ##       ##     ## ## ## ## ##   ####
+##       ##   ##   ##       ######### ##  #### ##    ##
+##       ##    ##  ##       ##     ## ##   ### ##    ##
+######## ##     ## ######## ##     ## ##    ##  ######
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Ruby') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########  ##     ## ########  ##    ##
+##     ## ##     ## ##     ##  ##  ##
+##     ## ##     ## ##     ##   ####
+########  ##     ## ########     ##
+##   ##   ##     ## ##     ##    ##
+##    ##  ##     ## ##     ##    ##
+##     ##  #######  ########     ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  } else if (now.lang === 'Bash') {
+                                    termEle.innerHTML += `
+                                    <div style="width: 100vh;height: 100vw">
+                                      <pre class="logo" style="color: #FFF">
+########     ###     ######  ##     ##
+##     ##   ## ##   ##    ## ##     ##
+##     ##  ##   ##  ##       ##     ##
+########  ##     ##  ######  #########
+##     ## #########       ## ##     ##
+##     ## ##     ## ##    ## ##     ##
+########  ##     ##  ######  ##     ##                                      
+                                      </pre>
+                                    </div>
+                                    `
+                                  }
+                                })
+                              }, 1000)
+                            }, 500)
+                          }, 1000)
+                        }, 500)
+                      }, 500)
+                    }, 500)
+                  }, 500)
+                })
+              })
+            })
+          })
+        })
+      })
+    }
+  }
+  next(0)
+})
---
+++ src/terminal.js
@@ -0,0 +1,203 @@
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this
+  return target.split(search).join(replacement)
+}
+
+const colorList = ['bg1', 'bg2', 'color1', 'color2']
+
+export class Terminal {
+  constructor(selector) {
+    this.buffer = []
+    this.insertHook = []
+    this.insertHook.push(this.render.bind(this))
+
+    
+    window.addEventListener('load', () => {
+      document.body.classList.add('bg1')
+      this.container = document.querySelector(selector)
+      this.render.apply(this)
+    })
+  }
+  insert(line) {
+    this.buffer.push(line)
+    this.insertHook.forEach(item => item())
+  }
+  render() {
+    const shadowEle = document.createElement('ul')
+    shadowEle.classList.add('bg1')
+    shadowEle.classList.add('terminalElement')
+
+    this.buffer.forEach(item => {
+      shadowEle.appendChild(item.render())
+    })
+
+    if (this.ele) {
+      this.ele.remove()
+    }
+    this.container.appendChild(shadowEle)
+    document.documentElement.scrollTop = document.documentElement.scrollHeight
+    this.ele = shadowEle
+  }
+}
+
+export class Line {
+  constructor(str) {
+    this.str = str
+    this.ele = document.createElement('li')
+  }
+  render() {
+    let replacedStr = this.str
+
+    for(let i = 0;i<colorList.length;i++) {
+      const color = colorList[i]
+      replacedStr = replacedStr.replaceAll(`<${color}>`, `<i class="${color}">`)
+      replacedStr = replacedStr.replaceAll(`</${color}>`, `</i>`)
+    }
+
+    this.ele.innerHTML = replacedStr
+    return this.ele
+  }
+}
+
+export class inputInterval extends Line {
+  constructor(PS1, text, fun) {
+    super("")
+    this.str = PS1 + " <color2>></color2> "
+    this.ele = document.createElement('li')
+
+    this.clicked = false
+
+    this.ele.onclick = () => {
+      this.clicked = true
+    }
+
+    let counter = 0
+    const updateStr = () => {
+      setTimeout(() => {
+        if (this.clicked) {
+          this.str = text
+          this.render()
+          if (fun) {
+            fun()
+          }
+        } else {
+          if (counter >= text.length - 1) {
+            this.str += text[counter++]
+            this.render()
+            if (fun) {
+              fun()
+            }
+          } else {
+            this.str += text[counter++]
+            this.render()
+            updateStr()
+          }
+        }
+      }, 10)
+    }
+    updateStr()
+  }
+  render() {
+    document.documentElement.scrollTop = document.documentElement.scrollHeight
+    let replacedStr = this.str
+    for(let i = 0;i<colorList.length;i++) {
+      const color = colorList[i]
+      replacedStr = replacedStr.replaceAll(`<${color}>`, `<i class="${color}">`)
+      replacedStr = replacedStr.replaceAll(`</${color}>`, `</i>`)
+    }
+
+    this.ele.innerHTML = replacedStr
+    return this.ele
+  }
+}
+
+
+
+export class ASCIIArt extends Line {
+  constructor(str) {
+    super("", {})
+    this.str = str
+  }
+  render() {
+    document.documentElement.scrollTop = document.documentElement.scrollHeight
+    const ele = document.createElement('li')
+    ele.innerHTML = this.str
+    return ele;
+  }
+}
+
+export class Choice extends Line {
+  constructor(PS1, content) {
+    super("", {})
+    this.PS1 = PS1
+    this.content = content
+
+    this.ele = document.createElement('li')
+    this.ele.style.display = "flex"
+
+    const PS = document.createElement('i')
+    PS.innerText = this.PS1
+    PS.classList.add('color1')
+
+    const Arrow = document.createElement('i')
+    Arrow.innerHTML = '&nbsp>&nbsp'
+    Arrow.classList.add('color2')
+
+    const choiceContainer = document.createElement('div')
+    for(let i = 0;i<this.content.length;i++) {
+      const item = this.content[i]
+
+      const choice = document.createElement('div')
+      choice.innerHTML = item
+      choice.classList.add('color2')
+      choice.classList.add('choice')
+      choice.onmouseover = () => {
+        choice.classList.add('bg2')
+      }
+      choice.onmouseleave = () => {
+        choice.classList.remove('bg2')
+      }
+      choice.onclick = () => {
+        choice.style.backgroundColor = "#4F5B66"
+        this.onChoose(i)
+      }
+      choiceContainer.appendChild(choice)
+    }
+
+    this.ele.appendChild(PS)
+    this.ele.appendChild(Arrow)
+    this.ele.appendChild(choiceContainer)
+  }
+  
+  render() {
+    document.documentElement.scrollTop = document.documentElement.scrollHeight
+    
+    return this.ele
+  }
+}
+
+export class Wait extends Line {
+  constructor(time, fun) {
+    super()
+    this.duration = time
+    const setPercent = (i) => {
+      if (i === 100) {
+        this.str = ""
+        if (fun) {
+          fun()
+        }
+      } else {
+        this.str = `${i}%`
+        document.documentElement.scrollTop = document.documentElement.scrollHeight
+        setTimeout(setPercent.bind(this, i+1), this.duration / 100)
+      }
+      this.render()
+    }
+    setPercent(0)
+    this.ele = document.createElement('li')
+  }
+  render() {
+    this.ele.innerHTML = this.str
+    return this.ele
+  }
+}
